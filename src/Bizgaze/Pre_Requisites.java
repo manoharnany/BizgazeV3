732f68895b17938675198c3f3439d708d3d73d73
Manoharnany <manohar.d@bizgaze.com>
2021-10-22 14:24:31 +0530

    add

--- src/Bizgaze/Pre_Requisites.java
+++ src/Bizgaze/Pre_Requisites.java
@@ -14,12 +14,11 @@ public class Pre_Requisites {
 	public String password1="";
 	public String username2="";
 	public String password2="";
-	public String create="";
-	public String update="";
-	public String search="";
-	public String save="";
-	public String close="";
-	
+	public String create="create-place d-flex";
+	public String update="biz-icon-size text-primary";
+	public String search="ListSearch";
+	public String save="btnDynamicSave";
+	public String close="btn_CloseForm";
 
 @BeforeClass
 	public void Startup() {
